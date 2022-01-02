57f7f7b7424ee355daad1fb8ce71fea8de0b59a0
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2022-01-02 10:57:52 +0700

    Update sahamapp.py

--- sahamapp.py
+++ sahamapp.py
@@ -12,7 +12,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import yfinance as yf # https://pypi.org/project/yfinance/
+#import yfinance as yf # https://pypi.org/project/yfinance/
 from ta.volatility import BollingerBands
 from ta.trend import MACD
 from ta.momentum import RSIIndicator
