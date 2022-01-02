17590a44a277e938634f34a81c1ae2b5846e989f
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2022-01-02 10:09:31 +0700

    Update sahamapp.py

--- sahamapp.py
+++ sahamapp.py
@@ -1,3 +1,18 @@
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
 # !pip install streamlit
 import streamlit as st
 import pandas as pd
